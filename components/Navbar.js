27940df55c087fc1e6475941c17dc45242bc4b38
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-08 19:10:21 +0530

    created navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from "../public/images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Navbar() {
-    return <div>This is Navbar</div>;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToogle = () => {
+        setIsOpen(isOpen ? false : true);
+    }
+
+    return (
+        <nav className='navbar'>
+            <div className="nav-center">
+                <div className="nav-header">
+                    <Link href="/" passHref>
+                        <Image src={logo} alt="HotelX" />
+                    </Link>
+                    <button className="nav-btn">
+                        <FaAlignRight className="nav-icon" onClick={handleToogle} />
+                    </button>
+                </div>
+                <ul className={isOpen ? 'nav-links show-nav' : 'nav-links'}>
+                    <li>
+                        <Link href="/" passHref>Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/rooms" passHref>Rooms</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
