3d933f1765df11405ee18dd9bd66c9775543d0c8
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-09 18:14:52 +0530

    created services

--- components/Services.js
+++ components/Services.js
@@ -3,10 +3,42 @@ import Title from "./Title";
 import { FaCocktail, FaShuttleVan, FaHiking, FaBeer } from "react-icons/fa"
 
 function Services() {
+    const services = [
+        {
+            icon: <FaCocktail />,
+            title: "Free Cocktails",
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quaerat!"
+        },
+        {
+            icon: <FaHiking />,
+            title: "Endless Hiking",
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quaerat!"
+        },
+        {
+            icon: <FaShuttleVan />,
+            title: "Free Shuttle",
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quaerat!"
+        },
+        {
+            icon: <FaBeer />,
+            title: "Strongest Beer",
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quaerat!"
+        },
+    ];
+
     return (
-        <div>
+        <section className='services'>
             <Title title="Services" />
-        </div>
+            <div className="services-center">
+                {services.map((item) => (
+                    <article key={item.title} className="service">
+                        <span>{item.icon}</span>
+                        <h6>{item.title}</h6>
+                        <p>{item.info}</p>
+                    </article>
+                ))}
+            </div>
+        </section>
     );
 }
 
