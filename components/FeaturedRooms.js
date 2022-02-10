c4db7642c0392ea093e194639a468808c972f445
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-10 18:49:34 +0530

    created featured rooms and Room

--- components/FeaturedRooms.js
+++ components/FeaturedRooms.js
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
 import { RoomContext } from '../Context/context';
 import Loading from "./Loading";
+import Room from './Room';
+import Title from './Title';
 
 function FeaturedRooms() {
-    const { featuredRooms: rooms } = useContext(RoomContext);
+    const { featuredRooms: rooms, loading } = useContext(RoomContext);
 
     return (
-        <div>
-            <Loading />
-        </div>
+        <section className='featured-rooms'>
+            <Title title="Featured Rooms" />
+            <div className="featured-rooms-center">
+                {loading ? <Loading />
+                    : rooms.map((room) => (
+                        <Room key={room.id} room={room} />
+                    ))}
+            </div>
+        </section>
     )
 }
 
---
+++ components/Room.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from "next/image";
+import defaultImage from "../public/images/room-1.jpeg";
+import Link from 'next/link';
+
+function Room({ room }) {
+    const { name, slug, images, price } = room;
+    return (
+        <article className='room'>
+            <div className="img-container">
+                <Image src={images[0] || defaultImage} alt="Room" />
+                <div className="price-top">
+                    <h6>${price}</h6>
+                    <p>per night</p>
+                </div>
+                <div className="btn-primary room-link">
+                    <Link href={`/rooms/${slug}`} className="btn-primary room-link">Features</Link>
+                </div>
+            </div>
+            <p className='room-info'>{name}</p>
+        </article>
+    )
+}
+
+export default Room
