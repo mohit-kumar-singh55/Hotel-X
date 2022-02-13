1a1b298efa5d16a67110ab4982e34222702ed692
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-13 20:07:19 +0530

    created handleChange

--- Context/context.js
+++ Context/context.js
@@ -40,11 +40,13 @@ class RoomProvider extends Component {
   }
 
   handleChange = (e) => {
-    const type = e.target.type;
+    const target = e.target;
+    const value = e.type === 'checkbox' ? target.checked : target.value;
     const name = e.target.name;
-    const value = e.target.value;
 
-    console.log(type, name, value);
+    this.setState({
+      [name]: value
+    }, this.filterRoom)
   }
 
   filterRoom = () => {
--- components/RoomsFilter.js
+++ components/RoomsFilter.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { RoomContext } from "../Context/context";
+import Title from "./Title";
 
-function RoomsFilter() {
+const getUnique = (items, value) => {
+    return [...new Set(items.map((item => item[value])))]
+}
+
+function RoomsFilter({ rooms }) {
+    const context = useContext(RoomContext);
+
+    const { handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets } = context;
+    // get unique types
+    let types = getUnique(rooms, "type");
+
+    // add all
+    types = ["all", ...types];
+
+    // map to jsx
+    types = types.map((item, index) => (
+        <option value={item} key={index}>{item}</option>
+    ))
+
     return (
-        <div>RoomsFilter</div>
+        <section className='filter-container'>
+            <Title title="Search Rooms" />
+            <form className='filter-form'>
+                {/* select type */}
+                <div className="form-group">
+                    <label htmlFor="type">Room Type</label>
+                    <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
+                        {types}
+                    </select>
+                </div>
+                {/* end of select type */}
+            </form>
+        </section>
     )
 }
 
