ef7016868a686fef6f7337fefc699b6e5754b1f0
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-13 20:45:56 +0530

    filtered room

--- Context/context.js
+++ Context/context.js
@@ -50,7 +50,17 @@ class RoomProvider extends Component {
   }
 
   filterRoom = () => {
-    console.log("Hello");
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } = this.state;
+
+    let tempRooms = [...rooms];
+
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+
+    this.setState({
+      sortedRooms: tempRooms
+    })
   }
 
   formatData(items) {
