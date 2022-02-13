c68422b3c8f5f7887eca86d2de3dc734fb8758ef
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-13 19:16:33 +0530

    changing context

--- Context/context.js
+++ Context/context.js
@@ -9,18 +9,46 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   }
 
   // get Data
   componentDidMount() {
     let rooms = this.formatData(items);
+
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
+  }
+
+  handleChange = (e) => {
+    const type = e.target.type;
+    const name = e.target.name;
+    const value = e.target.value;
+
+    console.log(type, name, value);
+  }
+
+  filterRoom = () => {
+    console.log("Hello");
   }
 
   formatData(items) {
@@ -37,7 +65,7 @@ class RoomProvider extends Component {
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, handleChange: this.handleChange }}>
         {this.props.children}
       </RoomContext.Provider>
     )
